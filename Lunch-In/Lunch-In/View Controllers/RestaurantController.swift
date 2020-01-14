aef190023585e0330661fc8067b42aab85dc7d00
John Holowesko <jholowesko@gmail.com>
2020-01-13 20:22:10 -0500

    Test Commit

--- Lunch-In/Lunch-In/View Controllers/RestaurantController.swift
+++ Lunch-In/Lunch-In/View Controllers/RestaurantController.swift
@@ -7,7 +7,6 @@ class RestaurantController {
 
     var suggestedRestaurants: [Restuarant] = []
     
-//    Restuarant(restaurantName: "John's Pizzaria", numerOfVotes: 0, previewImage: "pizza", didSelfVote: false), Restuarant(restaurantName: "Kevin's Wine", numerOfVotes: 10, previewImage: "wine", didSelfVote: false)
     
     
     // MARK: - Initializer:
